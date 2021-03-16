a64ddfda794921b3e927f06bc2c6205ace093474
Rashmil Sinha <rashmil.sinha@sleepnumber.com>
2021-03-16 11:45:24 -0700

    Updating CSS and adding slider

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -6,9 +6,6 @@ class Slide extends React.Component {
       <div className="testimonial-slide p-10 mr-3">
         <p>{this.props.bodyCopy}</p>
         <div className="credit pt-5">
-          <div className="image p-2">
-            <img src="https://source.unsplash.com/user/erondu" alt="unsplash" />
-          </div>
           <div className="meta">
             <p className="pb-2">{this.props.creditName}</p>
             <span>
